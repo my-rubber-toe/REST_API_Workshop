6bf062d107ee4449e6328326068c0a8a8fa2ebde
Roberto Y. Guzmán <roberto.guzman3@upr.edu>
2020-10-25 22:31:44 -0700

    Lesson 2: Create the main route

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def main_route():
+    """
+        Main route. Returns a list of the available endpoints on this service.
+    """
+    available_routes = {
+        0: '/',
+        1: '/movies',
+        2: '/movies/<id>',
+        3: '/movies?year=<int>&lt=<bool>&mt=<bool>',
+        4: '/movies?genre=<string>',
+        5: '/movies?runtime=<int>&lt=<bool>&mt=<bool>',
+        6: '/movies?servicename=<string>'
+    }
+
+    return jsonify(available_routes), 200
 
 
 if __name__ == "__main__":
