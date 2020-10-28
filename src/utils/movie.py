8f7275c873bac5a55cbba716836fb869d48c807f
Roberto Y. Guzmán <roberto.guzman3@upr.edu>
2020-10-27 21:09:43 -0700

    corrected lesson-2

--- src/utils/movie.py
+++ src/utils/movie.py
@@ -1,45 +1,52 @@
+  
 from bson import objectid
 
 
 class MovieModel:
     def __init__(self, **kwargs):
-        self.id: objectid = kwargs['_id']
-        self.title: str = kwargs['Title']
-        self.year: int = kwargs['Year']
-        self.age: int = kwargs['Age']
-        self.imdb: float = kwargs['IMDb']
-        self.rotten_tomatoes: str = kwargs['Rotten Tomatoes']
-        self.netflix: int = kwargs['Netflix']
-        self.hulu: int = kwargs['Hulu']
-        self.prime_video: int = kwargs['Prime Video']
-        self.disney_plus: int = kwargs['Disney+']
-        self.type = kwargs['Type']
-        self.directors: list = kwargs['Directors']
-        self.genres: list = kwargs['Genres']
-        self.country: str = kwargs['Country']
-        self.language: str = kwargs['Language']
-        self.runtime: int = kwargs['Runtime']
+
+        # Extract the keys from the database model
+        dictionary_keys = kwargs.keys()
+
+        # Dynamically set  properties of the movie if the key exists
+        self.id = str(kwargs['_id']) if dictionary_keys.__contains__('_id') else None
+        self.title = kwargs['title'] if dictionary_keys.__contains__('title') else None
+        self.year = kwargs['year'] if dictionary_keys.__contains__('year') else None
+        self.age = kwargs['age'] if dictionary_keys.__contains__('age') else None
+        self.imdb = kwargs['imdb'] if dictionary_keys.__contains__('imdb') else None
+        self.rotten_tomatoes = kwargs['rotten_tomatoes'] if dictionary_keys.__contains__('rotten_tomatoes') else None
+        self.netflix = kwargs['netflix'] if dictionary_keys.__contains__('netflix') else None
+        self.hulu = kwargs['hulu'] if dictionary_keys.__contains__('hulu') else None
+        self.prime_video = kwargs['prime_video'] if dictionary_keys.__contains__('prime_video') else None
+        self.disney_plus = kwargs['disney_plus'] if dictionary_keys.__contains__('disney_plus') else None
+        self.directors = kwargs['directors'] if dictionary_keys.__contains__('directors') else None
+        self.genres = kwargs['genres'] if dictionary_keys.__contains__('genres') else None
+        self.country = kwargs['country'] if dictionary_keys.__contains__('country') else None
+        self.language = kwargs['language'] if dictionary_keys.__contains__('language') else None
+        self.runtime = kwargs['runtime'] if dictionary_keys.__contains__('runtime') else None
+
+        # Best solution: Attributes must be accessed as the database attributes
+        # for k in kwargs.keys():
+        #     self.__setattr__(k, kwargs[k])
 
     def to_json(self):
-        
         return {
-            "_id": self.id,
-            "Title": self.title,
-            "Year": self.year,
-            "Age": self.age,
-            "IMDb": self.imdb,
-            "Rotten Tomatoes": self.rotten_tomatoes,
-            "Netflix": self.netflix,
-            "Hulu": self.hulu,
-            "Prime Video": self.prime_video,
-            "Disney+": self.disney_plus,
-            "Type": self.type,
-            "Directors": self.directors,
-            "Genres": self.genres,
-            "Country": self.country,
-            "Language": self.language,
-            "Runtime": self.runtime
+            "_id": str(self.id),
+            "title": self.title,
+            "year": self.year,
+            "age": self.age,
+            "imdb": self.imdb,
+            "rotten_tomatoes": self.rotten_tomatoes,
+            "netflix": self.netflix,
+            "hulu": self.hulu,
+            "prine_video": self.prime_video,
+            "disney_plus": self.disney_plus,
+            "directors": self.directors,
+            "genres": self.genres,
+            "country": self.country,
+            "language": self.language,
+            "runtime": self.runtime
         }
 
     def __str__(self):
-        return f"<MovieModel('Title':{self.title})>"
+        return f"<MovieModel('_id':{self.id})>"
