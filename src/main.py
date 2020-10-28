7d587e7771a34d4eac16b6aeaf2b5acb859484d4
Roberto Y. Guzmán <roberto.guzman3@upr.edu>
2020-10-27 21:10:46 -0700

    Corrected lesson-1

--- src/main.py
+++ src/main.py
@@ -1,32 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
-
-
-@app.route("/")
-def main_route():
-    """
-        Main route. Returns a list of the available endpoints on this service.
-    """
-    available_routes = {
-        0: '/',
-        1: '/movies',
-        2: '/movies/<id>',
-        3: '/movies?start=<int>&offset=<int>',
-    }
-
-    return jsonify(available_routes), 200
-
-
-@app.route("/movies")
-def movies_endpoint():
-    return "/movies"
-
-
-# Request parameter <id> can be set to be a specific numeric type int, float
-@app.route("/movies/<id>")
-def movies_by_id(id):  # Since we will be josking with bson we need it to be a str
-    return f"/movies/{id}"
 
 
 if __name__ == "__main__":
