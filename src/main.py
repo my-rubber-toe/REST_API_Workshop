4a374c340a259a40ef579bbffc44b870f3f2ccc6
Roberto Y. Guzmán <roberto.guzman3@upr.edu>
2020-11-03 20:36:38 -0800

    OK

--- src/main.py
+++ src/main.py
@@ -25,10 +25,8 @@ def main_route():
 @app.route("/movies")
 def movies_endpoint():
     if request.args:
-        start = int(request.args.get('start')
-                    ) if request.args.__contains__('start') else 0
-        offset = int(request.args.get('offset')
-                     ) if request.args.__contains__('start') else 0
+        start = int(request.args.get('start')) if request.args.get('start') else 0
+        offset = int(request.args.get('offset')) if request.args.get('offset') else 0
         return database.get_movies(start=start, offset=offset)
 
     return database.get_movies()
